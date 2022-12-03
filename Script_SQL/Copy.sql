8cc224d2238ba30aab1668ba2bdb62dedaccdbc4
ClaudeZhang7 <98498100+ClaudeZhang7@users.noreply.github.com>
2022-12-03 13:18:35 +0000

    Script Copy && Infos

---
+++ Script_SQL/Copy.sql
@@ -0,0 +1,17 @@
+-- table title_basics : 
+COPY title_basics FROM 'C:\Program Files\PostgreSQL\13\scripts\data_title_basics.tsv' csv DELIMITER ',' header  ;
+
+-- table name_basics : 
+COPY name_basics FROM 'C:\Program Files\PostgreSQL\13\scripts\data_name_basics.tsv' csv DELIMITER ',' header ;
+
+-- table title_crew :
+COPY title_crew FROM 'C:\Program Files\PostgreSQL\13\scripts\data_title_crew.tsv' csv DELIMITER ',' header ;
+
+-- table title_akas :
+COPY title_akas FROM 'C:\Program Files\PostgreSQL\13\scripts\data_title_akas.tsv' csv DELIMITER ',' header ;
+
+-- table title_episode :
+COPY title_episode FROM 'C:\Program Files\PostgreSQL\13\scripts\title_episode.tsv' csv DELIMITER E'\t' header  ;
+
+-- table title_ratings:
+COPY title_ratings FROM 'C:\Program Files\PostgreSQL\13\scripts\title_ratings.tsv' csv DELIMITER E'\t' header  ;
