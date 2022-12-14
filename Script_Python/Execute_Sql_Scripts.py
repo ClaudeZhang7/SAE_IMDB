92cb98efa7d64ce427c76de9823b9cd49b1dba39
ClaudeZhang7 <98498100+ClaudeZhang7@users.noreply.github.com>
2022-12-14 09:43:20 +0000

    update

--- Script_Python/Execute_Sql_Scripts.py
+++ Script_Python/Execute_Sql_Scripts.py
@@ -21,12 +21,14 @@ try:
     cur.execute(open("DropConstraints.sql", "r").read())
     cur.execute(open("CreateTable.sql", "r").read())
     cur.execute(open("Copy.sql", "r").read())
+    with open('title_principals.tsv','r',encoding='utf-8')as f: # on ouvre le fichier 
+        next(f) # on skip les lignes headers
+        cur.copy_from(f,'title_principals',sep='\t')  #on le copy à la table correspondante
+        
     cur.execute(open("Constraints.sql", "r").read())
-# On va pas utiliser les commandes ci-dessous car cela augmenterais de
-# manière significatif le temps de chargement mais on montre ici qu'on
-# à régler les problèmes liés aux "manques/mauvaises" données lié à IMDB
-    # cur.execute(open("DeleteFrom.sql", "r").read())
-    # cur.execute(open("ValidateConstraints.sql", "r").read())
+
+    cur.execute(open("DeleteFrom.sql", "r").read())
+    cur.execute(open("ValidateConstraints.sql", "r").read())
     conn.commit()  
 
     
