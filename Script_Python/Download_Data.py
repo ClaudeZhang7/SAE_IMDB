94f8a1127604350efa5ff819481f94998caf979d
ClaudeZhang7 <98498100+ClaudeZhang7@users.noreply.github.com>
2023-01-15 09:22:57 +0000

    update

--- Script_Python/Download_Data.py
+++ Script_Python/Download_Data.py
@@ -3,6 +3,8 @@ import wget
 import gzip
 import shutil
 import pandas as pd # règle le pb de data missing for column
+import subprocess
+import os
 
 link = input("Quelle est le lien pour télécharger les fichier ?\n")
 html = requests.get(link).text
@@ -49,3 +51,24 @@ while y<count :
 df = pd.read_csv('title.basics.tsv', delimiter='\t')
 df = df.dropna(subset=['genres'])
 df.to_csv('title.basics.tsv', sep='\t', index=False) # chemin à mettre poto 
+
+# on donne toutes les permissions aux != fichiers
+permissions = os.stat("title.basics.tsv").st_mode
+print(permissions)
+os.chmod("title.basics.tsv", 0o777)
+permissions = os.stat("title.basics.tsv").st_mode
+print(permissions)
+
+permissions = os.stat("title.basics.tsv").st_mode
+
+os.chmod("title.akas.tsv", 0o777)
+os.chmod("title.crew.tsv", 0o777)
+os.chmod("title.episode.tsv", 0o777)
+os.chmod("title.principals.tsv", 0o777)
+os.chmod("title.ratings.tsv", 0o777)
+os.chmod("name.basics.tsv", 0o777)
+
+# execute le fichier python
+subprocess.run(["python", "Execute_Sql_Scripts.py"])
+
+
