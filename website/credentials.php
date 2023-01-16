3308163f2eb006adcdae85de801ee5d88bde50b1
Samuel-DD07 <samueldorismond@gmail.com>
2023-01-16 11:08:56 +0100

    ok

---
+++ website/credentials.php
@@ -0,0 +1,7 @@
+<?php 
+    $host = "localhost";
+    $driver = "pgsql";
+    $dbname = "php_exercises";
+    $port = "5433";
+    $dsn = "$driver:host=$host;port=$port;dbname=$dbname";
+?>
--- website/index.php
+++ website/index.php
@@ -5,33 +5,10 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
-    <title>test du block amazon prime</title>
+    <title>Apna.TV - Plateforme de streaming powered by IMDB</title>
 </head>
 <body>
-    <?php require_once "./components/header.php" ?>
     <section>
-        <h3>Films</h3>
-        <div class="wrap">
-            <?php
-                // require_once './Function/Api.php';
-                // get_five_poster(1);
-            ?>
-    </section>
-    <section>
-        <h3>Series</h3>
-        <div class="wrap">
-            <?php
-                // require_once './Function/Api.php';
-                // get_five_poster(6);
-            ?>
-    </section>
-    <section>
-        <h3>Documentaires</h3>
-        <div class="wrap">
-            <?php
-                // require_once './Function/Api.php';
-                // get_five_poster(11);
-            ?>
     </section>
 </body>
 </html>
